0652b72a5cad44ac9c5ff73264e8c110141bb7b1
Aster Santana <50426897+astersantana@users.noreply.github.com>
2023-02-10 14:08:23 -0300

    feature: configure deployment of the first version of the app

--- relief_planning/__init__.py
+++ relief_planning/__init__.py
@@ -5,3 +5,23 @@ from relief_planning.main import solve
 # For a configured deployment on Mip Hub see:
 # https://github.com/mipwise/mip-go/tree/main/6_deploy/4_configured_deployment
 
+input_tables_config = {
+    'hidden_tables': ['parameters'],
+    'categories': {
+        'Master Data': ['products', 'suppliers', 'relief_camps'],
+        'Supply and Demand': ['products_suppliers', 'shipping_costs', 'products_demands']
+    },
+    'order': [],
+    'tables_display_names': {},
+    'columns_display_names': {},
+    'hidden_columns': {}
+    }
+
+output_tables_config = {
+    'hidden_tables': [],
+    'categories': {},
+    'order': [],
+    'tables_display_names': {},
+    'columns_display_names': {},
+    'hidden_columns': {}
+    }
